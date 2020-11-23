44b3726cd8cac8f53cddf34f309ad29c8d548137
Gustavo Camello <gucamello@gmail.com>
2020-11-23 14:28:49 +0000

    create form component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,17 @@
 import React from "react";
 import NavBar from "./NavBar";
+import Form from "./Form";
+import "./App.css";
 
 
 class App extends React.Component {
   render () {
     return (
       <div className="App">
-        <NavBar />
+        <>
+          <NavBar />
+          <Form />
+        </>
       </div>
       )
   }
