b1d91d8171134f1504409db4bb1a461513dbce81
Gustavo Camello <gucamello@gmail.com>
2020-11-23 14:39:25 +0000

    create themeContext

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React from "react";
 import NavBar from "./NavBar";
+import PageContent from "./PageContent";
 import Form from "./Form";
 import "./App.css";
+import { ThemeProvider } from "./contexts/ThemeContext";
 
 
 class App extends React.Component {
   render () {
     return (
-      <div className="App">
-        <>
+      <ThemeProvider>
+        <PageContent>
           <NavBar />
           <Form />
-        </>
-      </div>
+        </PageContent>
+      </ThemeProvider>
       )
   }
 }
